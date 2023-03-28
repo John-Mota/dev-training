5b0deb4088c047ee2e17d9fbc40fc53b4d30e637
John Mota <johnmarcos.mota@gmail.com>
2023-03-28 12:20:46 -0300

    implementando delete e o patch

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Delete, Patch } from '@nestjs/common/decorators';
 import { AppService } from 'src/app.service';
 
 @Controller('courses')
@@ -26,5 +27,15 @@ export class CoursesController {
     @HttpCode(HttpStatus.NO_CONTENT)
     create(@Body('description') body) {
         return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return `Atualização Curso ${id}`
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): string{
+        return `Curso ${id} Excluído`;
     }
 }
