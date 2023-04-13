a69154bc50626af09cdf77f392ef6b2b97505a24
John Mota <johnmarcos.mota@gmail.com>
2023-04-13 03:12:05 -0300

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [ CoursesModule, TypeOrmModule.forRoot({
+  imports: [ CoursesModule, ConfigModule.forRoot() ,TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'db',
+    host: 'localhost',
     port: 5432,
     username: 'postgres',
     password: 'docker',
     database: 'cursonestjs',
     autoLoadEntities: true,
-    synchronize: false,
+    synchronize: true,
     entities: []
   })],
   controllers: [AppController],
